10dc5cce124a9a2281b119615246b796d9ca0566
Jim Thomson <jthomson@apl.washington.edu>
2025-11-06 17:04:26 +0000

    Update Tchain_RBR.m
    Coastal Freeze version
    *** still not using pressure correctly ***

--- Tchain_RBR.m
+++ Tchain_RBR.m
@@ -1,7 +1,7 @@
 % Bulk Read RBR Solo and Duo loggers on a T-chain
 % 
-% Michael James
-% 06/27/2024
+% Michael James, 06/27/2024
+% J. Thomson, modify for single pressure, 11/2025
 % 
 
 clc, clear, close all
@@ -10,34 +10,35 @@ clc, clear, close all
 % and desired time start/stop. Z must be manually inputted since RBR solos
 % do not have data on depth/position
 
-t1 = 7.394247886921297e5; % time start
-t2 = 7.394299614814814e5; % time end
+atmospres = 10;  % dB
+
+t1 = datenum(2025,10,28,7,0,0); % time start
+t2 = datenum(2025,11,5,18,0,0); % time end
 
 % Input SN of RBR here IN ORDER of associated z below.
-serial_list = [212832;212833;212834;212857;212858;212859;212860;212861;212862; ... 
-    212863;212864;200488;212865;212866;212867;212868;212869;212870;...
-    212871;212872;200492;212873;212874;212875;212876;212877;212878;...
-    212879;212880; 212881 ;200495];
+serial_list = [212832;212833;212834;212857;212858;212859;212860;212861;212862;240148; ... 
+    212863;212864;212865;212866;212867;212868;212869;212870;...
+    212871;212872;212873;];
 
 % Input Tchain lengths here. 
-z = [0;0.25;0.5;0.75;1;1.25;1.5;1.75;2;...
-    2.25;2.5;2.75;3.25;3.5;3.75;4;4.25;4.5;...
-    4.75;5;5.25;5.75;6.25;6.75;7.25;7.75;8.25;8.75;9.25;9.75;10;];
+z = [0:2:40];
 
 % Distance from surface to first node. 
-buoyoffset = 0.5; % This is an additional length to offset your first RBR in case you have slack between it and surface.
+buoyoffset = 0; % This is an additional length to offset your first RBR in case you have slack between it and surface.
 z = buoyoffset + z;
 
+% force the last sensor to be on the bottom (depth same as anchor)
+anchordepth = 20.5;
 
 
 %% Loading loop
 
 % Need RSK tools library from RBR
 
-rawdatapath = [uigetdir()]; % Calls explorer to get directory.  
-% rawdatapath = []; % Can also input raw dir here. 
+%rawdatapath = [uigetdir()]; % Calls explorer to get directory.  
+rawdatapath = pwd % Can also input raw dir here. 
 
-files = dir(fullfile(rawdatapath, '*.rsk*')); % Use this to filter your files
+files = dir(fullfile(rawdatapath, '/*.rsk*')); % Use this to filter your files
 
 for fi = 1:length(files)
     fid = RSKopen(fullfile(files(fi).folder, files(fi).name));
@@ -48,33 +49,38 @@ for fi = 1:length(files)
     Tchain.temp(idx,:) = rsk.data.values(:,1)'; %C
     try
         Tchain.P(idx,:) = rsk.data.values(:,2)'; %dbar
-        disp('duo processed')
+        disp('pressure data found and processed')
     catch 
-        disp('solo processed')
+        disp('solo temperature only processed')
         Tchain.P(idx,:) = nan(size(rsk.data.values(:,1)'));
     end; clear rsk;
 end;clear fi fid idx;
 
+% force last sensor to be on the bottom
+Tchain.P(size(Tchain.P,1),:) = anchordepth + atmospres; 
+
 
 %% Interpolate z to depth (Linear Fit)
 if any(~isnan(Tchain.P))
-    disp('Duos present, interpolating depth to P')
+    disp('Duos / Duets present, interpolating depth to P')
     % P1 Subtract absolute pressure as needed (Assuming we're in the top 10 m)
     [Pmin, r] = min(Tchain.P,[], "all"); % Calculate Min
     [r, ~] = find(Tchain.P == Tchain.P(r)); % Grab Row
     if abs(z(r)-Pmin) > 0.1*z(r) % examine offset % 
-        Tchain.P  = Tchain.P -10; % Remove 1 bar atmospheric
+        Tchain.P  = Tchain.P - atmospres; % Remove  atmospheric
     end
     
     %% P2 linear interp Assuming dbar = 1m depth
+    % edit this block for variable number of pressure sensors 
+
     [Prows ~] = find(~isnan(Tchain.P(:,1))) %Assume consistent
     depth = nan(size(Tchain.P));
     for i =1:length(Tchain.time)
         Stretch_ratio = Tchain.P(Prows,i) ./ z(Prows); % Linear Stretching between each P point
         % Stretch_ratio = Stretch_ratio(~isnan(Stretch_ratio));
         depth(1:Prows(1),i) = Stretch_ratio(1).*z(1:Prows(1));
+        %depth((Prows(1)+1):length(z),i) = Stretch_ratio(2).*z((Prows(1)+1):end);
         depth(Prows(1)+1:Prows(2),i) = Stretch_ratio(2).*z(Prows(1)+1:Prows(2));
-        depth(Prows(2)+1:Prows(3),i) = Stretch_ratio(3).*z(Prows(2)+1:Prows(3));
     end;clear i;
 else 
     disp('All Solos')
@@ -82,40 +88,38 @@ end;
     
 
 %% Plot pcolor of Tchain
-figure;
+
 
 % z vs P to examine stretch
+figure(1)
 plot(mean(Tchain.P, 2), z, 'k.')
 xlabel('Pressure [dbar]'); ylabel('Length Along tether unstretched [m]')
 axis manual
-l = [min(min(Tchain.P,[], "all"), min(z)), max(max(Tchain.P,[], "all"), max(z))
+l = [min(min(Tchain.P,[], "all"), min(z)), max(max(Tchain.P,[], "all"), max(z))];
 plot(l, l, 'k--')
 
-figure;
+
 
 limc = round([nanmean(Tchain.temp,'all') - 2*nanstd(Tchain.temp,[],'all'), ...
                nanmean(Tchain.temp,'all') + 2*nanstd(Tchain.temp,[],'all')]);
 
-
-tiledlayout("vertical")
-nexttile;
+figure(2)
 pcolor(Tchain.time, depth, Tchain.temp)
 set(allchild(gca), 'EdgeAlpha', 0)
-caxis(limc); colormap(cmocean('thermal'));ylabel(colorbar,'[deg C]'
+caxis(limc); colormap(cmocean('thermal')); ylabel(colorbar,'[deg C]')
 ylabel('Pressure [dbar]'); xlabel('[UTC]'); datetick;axis tight;axis ij;
 
-nexttile;
+figure(3)
 pcolor(Tchain.time, z, Tchain.temp)
 set(allchild(gca), 'EdgeAlpha', 0)
 caxis(limc); colormap(cmocean('thermal'));ylabel(colorbar,'[deg C]')
 ylabel('Length Along tether unstretched [m]'); xlabel('[UTC]'); datetick;axis tight;axis ij;
 
-linkaxes(findobj(allchild(gcf), 'Type', 'Axes'),'xy');
+%linkaxes(findobj(allchild(gcf), 'Type', 'Axes'),'xy');
 
-name = [rawdatapath , ' Tchain'];
-sgtitle(name)
-
-savefig(name)
+%name = [rawdatapath , ' Tchain'];
+%sgtitle(name)
+%savefig(name)
 
 
 
